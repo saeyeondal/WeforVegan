81c9e81da83af060ce5e81b365e77f138546d899
jihyeon <nsa06120@naver.com>
2020-09-04 14:37:16 +0900

    필요없는 코드 삭제

--- app/src/main/java/com/example/weforvegan/LikeRecipePage.java
+++ app/src/main/java/com/example/weforvegan/LikeRecipePage.java
@@ -25,33 +25,7 @@ public class LikeRecipePage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.likerecipe_page, container, false);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                try{
-                    final ImageView iv1 = (ImageView)rootView.findViewById(R.id.imageView1);
-                    final ImageView iv2 = (ImageView)rootView.findViewById(R.id.imageView2);
-                    final ImageView iv3 = (ImageView)rootView.findViewById(R.id.imageView3);
-                    URL url = new URL("https://recipe1.ezmember.co.kr/cache/recipe/2019/03/05/d2e237f1f404e0cfefa723400c63937e1.jpg");
-                    InputStream is = url.openStream();
-                    final Bitmap bm = BitmapFactory.decodeStream(is);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            iv1.setImageBitmap(bm);
-                            iv2.setImageBitmap(bm);
-                            iv3.setImageBitmap(bm);
-                        }
-                    });
-                    iv1.setImageBitmap(bm); //비트맵 객체로 보여주기
-                    iv2.setImageBitmap(bm);
-                    iv3.setImageBitmap(bm);
-                } catch(Exception e){
-                }
-            }
-        });
-        t.start();
+
         return rootView;
     }
 }
