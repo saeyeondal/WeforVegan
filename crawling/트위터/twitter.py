e85efa33dd5f8298e68829a9acbd36f07e91a67d
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2020-09-06 01:35:55 +0900

    twitter_test

--- crawling/트위터/twitter.py
+++ crawling/트위터/twitter.py
@@ -1,14 +1,14 @@
 import tweepy
 
 # 트위터 앱의 Keys and Access Tokens 탭 참조(자신의 설정 값을 넣어준다)
-consumer_key =  ''
-consumer_secret = ''
+consumer_key = 'QngRSRbxMEPDisG3aNodxh5vJ'
+consumer_secret = 'hloMOXz9xtAR3XJxxISoBNdOJSdP8otEYu7n6SQEv8RwfAcKub'
 
 # 1. 인증요청(1차) : 개인 앱 정보 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
-access_token = ''
-access_token_secret= ''
+access_token = '1302255191052304384-NfWUxPx7dStMMA3q4u4J1lPToZ1H3k'
+access_token_secret= 'rcxbVQm6cs98oTwxOVYw5foMJ03cp7poC6dANmk4PJ1sB'
 
 # 2. access 토큰 요청(2차) - 인증요청 참조변수 이용
 auth.set_access_token(access_token, access_token_secret)
@@ -16,12 +16,18 @@ auth.set_access_token(access_token, access_token_secret)
 # 3. twitter API 생성  
 api = tweepy.API(auth)
 
+location = "%s,%s,%s" % ("35.95", "128.25", "1000km")
+#vegan_recipe_
+keyword = "비건레시피";     # 자신이 검색하고 싶은 키워드 입력 
+search = [] # 크롤링 결과 저장할 변수   
 
-keyword = "비건";     # 검색할 키워드
+# twitter 검색 cursor 선언
+# tweepy api 는 일주일만 가능?
+cursor = tweepy.Cursor(api.search, q=keyword,
+                       since='2018-01-01', # 2018-01-01 이후에 작성된 트윗들로 가져옴
+                       count=20, # 페이지당 반환할 트위터 수 최대 20
+                       geocode=location, # 검색 반경 조건
+                       include_entities=True)
 
-tweets = api.search(keyword) # 한 번에 15 트윗 검색 정보를 가져옴
-# tweets = api.search(keyword, count=100) # 일반 계정으로는 최대 100개 최신 게시물을 가져올 수 있음
-for num, tweet in enumerate(tweets):
-    print(tweet.text)
-    print (tweet.favorite_count)
-    print (tweet.retweet_count)
+for i, tweet in enumerate(cursor.items()):
+    print("{}: {}".format(i, tweet.text))
