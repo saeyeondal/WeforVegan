536de7c9091d4fe0c3f5df6b7062a571b87e1888
hhaggang22 <20181018@sungshin.ac.kr>
2020-10-13 23:03:38 +0900

    레시피 검색

---
+++ server/SearchRecipe.php
@@ -0,0 +1,63 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<?php
+session_start();
+
+header("Content-Type:text/html;charset=utf8");
+
+$con=mysqli_connect("dbinstance.c4nf0uecennm.us-east-2.rds.amazonaws.com", "weforvegan", "sungshin18" , "weforvegan");
+
+if(mysqli_connect_errno($con))
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_errno();
+}
+
+mysqli_set_charset($con, "utf8");
+
+$search_key='%'.$_POST['recipe'].'%';
+$recipe_sql = "SELECT * FROM recipe WHERE rp_name LIKE '{$search_key}'" ;
+$res = mysqli_query($con, $recipe_sql) or die(mysqli_error($con));
+
+$result = array();
+
+while($row = mysqli_fetch_array($res)){
+
+		array_push($result, 
+		array('rp_idx'=>$row[0], 'rp_source'=>$row[2]
+		));
+	}
+
+
+$snsrecipe_sql = "SELECT * FROM snsrecipe WHERE recipe_rp_name LIKE '{$search_key}'" ;
+$apirecipe_sql = "SELECT * FROM apirecipe WHERE recipe_rp_name LIKE '{$search_key}'" ;
+
+$snsres = mysqli_query($con, $snsrecipe_sql) or die(mysqli_error($con));
+$apires = mysqli_query($con, $apirecipe_sql) or die(mysqli_error($con));
+
+$finalresult = array();
+
+if($row[2] = 'twitter' or $row[2]= 'instagram' or $row[2] = '10000 recipe'){
+while($snsrow = mysqli_fetch_array($snsres)){
+
+		array_push($finalresult, 
+		array('sns_url'=>$snsrow[0], 'sns_imgurl'=>$snsrow[1]
+		));
+	}
+
+}
+else if(	$row[2]= 'api'){
+while($apirow = mysqli_fetch_array($apires)){
+
+		array_push($finalresult, 
+		array('api_category'=>$apirow[0], 'api_imgurlsmall'=>$apirow[6], 'api_imgurlbig'=>$apirow[7], 'api_recipe'=>$apirow[8], 'api_ingredient'=>$apirow[9]
+		));
+	}
+
+} 
+
+echo json_encode(array("finalresult"=>$finalresult), JSON_UNESCAPED_UNICODE);
+
+
+mysqli_close($con);
+
+?>
---
+++ server/text2.php
@@ -0,0 +1,26 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+ 
+
+<html>
+
+<title>TEST 1</title>
+
+<body>
+
+
+<form action="SearchRecipe.php" method="post">      
+
+<input type = text  name = recipe>
+
+<input type = "submit" value = " recipe search">
+
+</form>
+
+
+
+ 
+
+</body>
+
+</html>
