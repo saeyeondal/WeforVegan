e24a4ab24919a64e0ef9bce2211afbaa7a94e653
jihyeon <nsa06120@naver.com>
2020-11-06 10:05:28 +0900

    회원정보 수정 중 - 아이디 중복 말고 다 한 듯

--- app/src/main/java/com/example/weforvegan/JsonParser.java
+++ app/src/main/java/com/example/weforvegan/JsonParser.java
@@ -15,4 +15,26 @@ public class JsonParser {
         }
         return result;
     }
+
+    public static String[] inform_parse(String jsonString){
+        String[] userArray = null;
+
+        try{
+            JSONArray jsonArray = new JSONObject(jsonString).getJSONArray("message");
+            userArray = new String[7];
+            for(int i=0; i<jsonArray.length(); i++) {
+                JSONObject location = jsonArray.getJSONObject(i);
+                userArray[0] = location.optString("usr_id");
+                userArray[1] = location.optString("usr_pw");
+                userArray[2] = location.optString("usr_name");
+                userArray[3]= location.optString("usr_sex");
+                userArray[4]= location.optString("usr_birth");
+                userArray[5]= location.optString("usr_vegantype");
+                userArray[6]= location.optString("usr_likeidx");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return userArray;
+    }
 }
