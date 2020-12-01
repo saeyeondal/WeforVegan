4e2091c61a24d5c75a266de96903e307a21a9cc9
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2020-12-02 01:51:37 +0900

    Update insta.py

--- crawling/인스타그램/insta.py
+++ crawling/인스타그램/insta.py
@@ -11,8 +11,8 @@ def move_next(driver):
     right.click()
     time.sleep(3)
 
-conn = pymysql.connect(host = 'dbinstance.c4nf0uecennm.us-east-2.rds.amazonaws.com', user = 'weforvegan',
-                       password = 'sungshin18', db = 'weforvegan', charset = 'utf8')
+conn = pymysql.connect(host = 'ㅇㅇ', user = 'ㅇㅇ',
+                       password = 'ㅇㅇ', db = 'ㅇㅇ', charset = 'utf8')
 cursor = conn.cursor()
 
 driver = webdriver.Chrome() #크롬을 이용
